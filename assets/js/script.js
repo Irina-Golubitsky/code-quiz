a5702ac8eb1227c3c9f7901899b62552b68e54b9
Irina-Golubitsky <irina.golubitsky@gmail.com>
2021-07-09 20:15:39 -0700

    html - start quiz; css for start quiz; js - array of questions

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+var questionArray = [
+    {question: 'Why is it important to be careful of the source when embedding an <iframe>?',
+    choises: ['Copyright infringement','Security risks from “bad” websites', 'All of the above'],
+    answer:3
+    },
+    {question: 'I wish to align an element to the top of its container, which CSS property should I use?',
+    choises: ['text-align: top;','margin-align: top;', 'overflow: top;','vertical-align: top;'],
+    answer:4
+    },
+    {question: 'The main purpose of the <head> element is that it contains information that helps the browser understand what the page is about, what it should look like, and any other behind-the-scenes information.',
+    choises: ['true','false'],
+    answer:1
+    },
+    {question: "The main purpose of the <body> element is to hold all of the document's actual content that is meant to be seen or interacted with by the page's visitor",
+    choises: ['true','false'],
+    answer:1
+    },
+    {question: "Which of the following is NOT a good reason for version control?",
+    choises: ['Version control allows the codebase to be modified and tested without interrupting the user experience.','Version control allows changes to the codebase to be tested individually.', 
+    'Version control allows teams to work on individual features synchronously.', 'Version control allows features to ship directly to the main branch.'],
+    answer:4
+    },
+    {question: "We are currently on the develop branch. Which of the following commands does NOT switch to a new branch?",
+    choises: ['git checkout main','git checkout -b feature/header', 'git branch feature/header'],
+    answer:3
+    },
+    {question: "After you’re done creating and testing a new feature in a feature branch, what is the next step?",
+    choises: ['Merge the feature branch into the main branch.','Merge the feature branch into the develop branch.', 'Create a new feature branch.'],
+    answer:2
+    },
+    {question: "How do you create a flexbox?",
+    choises: ['display: flex;','display: flexbox;', 'display: box;'],
+    answer:1
+    },
+    {question: "By default, in which direction does a flexbox lay out its items?",
+    choises: ['A row (horizontal), with all of the child elements laid out side by side.','A column (vertical), with all of the child elements laid out on top of one another.'],
+    answer:1
+    },
+    {question: "What do media queries allow us to do?",
+    choises: ['Play videos on our page.','Create responsive designs.', 'Change CSS at different browser widths.'],
+    answer:3
+    },
+    {question: "Which one of these is NOT a valid media type for media queries?",
+    choises: ['all','screen', 'speech','tablet'],
+    answer:4
+    }
+]
+
+
+
+  
